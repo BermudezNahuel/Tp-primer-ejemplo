71cada66a0382def0579644fe806ce8390a3d851
BermudezNahuel <bermudez93na@gmail.com>
2023-10-02 20:36:44 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using biblioteca_compartida;
+
+namespace Tp_primer_ejemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var admin_1 = new Administrador(1, 1);
+
+            //Console.WriteLine(admin_1.MostrarInfo());
+
+            var alumno_1 = new Estudiante("1234", "juan","corrales", 1234, "juan23qgmail.com", "AV corodba 145", new DateTime(1995, 06, 4), "12345678", 0001);
+
+            Console.WriteLine(alumno_1.MostrarInfo());
+
+            var numero = GestorUsuarios.GetRandomNumber();
+
+            Console.WriteLine(numero);
+            //Console.WriteLine(alumno_1.MostrarInfo());
+
+
+            //r materia1 = Datos.Abrir().Materias; //--> retorna un objeto de clase maestra
+
+
+            /*
+            foreach (var curso in materia1)
+            {
+                if (curso.Curso == 0)
+                {
+
+                    Console.WriteLine("Año: Primero");
+                    Console.WriteLine("Materias: ");
+
+                    foreach (var mat in curso.Materias)
+                    {
+                        Console.WriteLine(mat);
+                    }
+
+                }
+
+            }
+            */
+
+            var direccion = Directorios.ObtenerDirectorio();
+            Console.WriteLine(direccion);
+
+        }
+    }
+}
