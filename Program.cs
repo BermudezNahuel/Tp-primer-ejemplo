851ed360107aa7364f55c831eb85eff7e95638d2
BermudezNahuel <bermudez93na@gmail.com>
2023-10-03 22:01:13 -0300

    cambio form

--- Program.cs
+++ Program.cs
@@ -10,40 +10,60 @@ namespace Tp_primer_ejemplo
 
             //Console.WriteLine(admin_1.MostrarInfo());
 
-            var alumno_1 = new Estudiante("1234", "juan","corrales", 1234, "juan23qgmail.com", "AV corodba 145", new DateTime(1995, 06, 4), "12345678", 0001);
+            //var alumno_1 = new Estudiante("1234", "juan","corrales", 1234, "juan23qgmail.com", "AV corodba 145", new DateTime(1995, 06, 4), "12345678", 0001);
 
-            Console.WriteLine(alumno_1.MostrarInfo());
-
-            var numero = GestorUsuarios.GetRandomNumber();
-
-            Console.WriteLine(numero);
             //Console.WriteLine(alumno_1.MostrarInfo());
 
+            //var numero = GestorUsuarios.GetRandomNumber();
 
-            //r materia1 = Datos.Abrir().Materias; //--> retorna un objeto de clase maestra
+            //Console.WriteLine(numero);
+            ////Console.WriteLine(alumno_1.MostrarInfo());
 
 
-            /*
-            foreach (var curso in materia1)
+            ////r materia1 = Datos.Abrir().Materias; //--> retorna un objeto de clase maestra
+
+
+            ///*
+            //foreach (var curso in materia1)
+            //{
+            //    if (curso.Curso == 0)
+            //    {
+
+            //        Console.WriteLine("Año: Primero");
+            //        Console.WriteLine("Materias: ");
+
+            //        foreach (var mat in curso.Materias)
+            //        {
+            //            Console.WriteLine(mat);
+            //        }
+
+            //    }
+
+            //}
+            //*/
+
+            //var direccion = Directorios.ObtenerDirectorio();
+            //Console.WriteLine(direccion);
+
+            Console.WriteLine("*********************************");
+            var datos = Datos.Abrir();
+            List<Administrador> listaAdministradores = datos.Administradores;
+
+            var admin = new Administrador(2, 2);
+
+            Console.WriteLine(admin.MostrarInfo());
+
+            listaAdministradores.Add(admin);
+
+
+            foreach (var item in listaAdministradores)
             {
-                if (curso.Curso == 0)
-                {
-
-                    Console.WriteLine("Año: Primero");
-                    Console.WriteLine("Materias: ");
-
-                    foreach (var mat in curso.Materias)
-                    {
-                        Console.WriteLine(mat);
-                    }
-
-                }
-
+                Console.WriteLine(item.MostrarInfo());
             }
-            */
 
-            var direccion = Directorios.ObtenerDirectorio();
-            Console.WriteLine(direccion);
+            Datos.Guardar(datos);
+
+
 
         }
     }
